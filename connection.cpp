f5f8348d2657c70e43da46761b09739050b041f3
youssef <youssef.ayadi@esprit.tn>
2023-03-01 20:56:43 +0100

    first commit

---
+++ connection.cpp
@@ -0,0 +1,22 @@
+#include "connection.h"
+
+connection::connection()
+{
+
+}
+bool connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("Source_Project2A");
+db.setUserName("system");//inserer nom de l'utilisateur
+db.setPassword("oracle");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+
+
+
+    return  test;
+}
